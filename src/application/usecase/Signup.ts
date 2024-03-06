826b5a3907dcfd7ebe5f3301ace19ede09d4d56f
Edenilson Souza <edenilson.sza@gmail.com>
2024-03-06 18:22:50 -0300

    feat: adiciona caso de uso Signup

---
+++ src/application/usecase/Signup.ts
@@ -0,0 +1,20 @@
+import MailerGateway from "../../infra/gateway/MailerGateway";
+import Account from "../../domain/entity/Account";
+import AccountRepository from "../../infra/repository/AccountRepository";
+
+// Use case
+export default class Signup {
+    constructor(readonly accountRepository: AccountRepository, readonly mailerGateway: MailerGateway) {}
+
+    async execute(input: any) {
+        const existingAccount = await this.accountRepository.getByEmail(input.email);
+        if (existingAccount) throw new Error("Account already exists");
+
+        const account = Account.create(input.name, input.email, input.phone);
+        await this.accountRepository.save(account);
+        await this.mailerGateway.send("Welcome", account.getEmail(), "Use this link to confirm your account");
+        return {
+            accountId: account.accountId
+        };
+    }
+}
