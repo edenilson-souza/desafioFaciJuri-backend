d46f41304f9304e643eaf7034821768c748494ab
Edenilson Souza <edenilson.sza@gmail.com>
2024-03-06 18:22:14 -0300

    test: adiciona teste do objeto de valor de telefone

---
+++ test/unit/Phone.test.ts
@@ -0,0 +1,9 @@
+import Phone from "../../src/domain/vo/Phone";
+
+test.each(["97456321558", "71428793860", "87748248800"])("Deve testar se o telefone é válido: %s", function (phone: string) {
+    expect(new Phone(phone)).toBeDefined();
+});
+
+test.each(["8774824880", null, undefined, "11111111111"])("Deve testar se o telefone é inválido: %s", function (phone: any) {
+    expect(() => new Phone(phone)).toThrow(new Error("Invalid phone"));
+});
