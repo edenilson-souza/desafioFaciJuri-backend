366e29461644875ce916796965f69397dfeeb608
Edenilson Souza <edenilson.sza@gmail.com>
2024-03-06 18:23:06 -0300

    feat: adiciona caso de uso GetAccount

---
+++ src/application/usecase/GetAccount.ts
@@ -0,0 +1,16 @@
+import AccountRepository from "../../infra/repository/AccountRepository";
+
+export default class GetAccount {
+    constructor(readonly accountRepository: AccountRepository) {}
+
+    async execute(accountId: string) {
+        const account = await this.accountRepository.getById(accountId);
+        if (!account) throw new Error("Account does not exist");
+        return {
+            accountId: account.accountId,
+            name: account.getName(),
+            email: account.getEmail(),
+            phone: account.getPhone()
+        };
+    }
+}
