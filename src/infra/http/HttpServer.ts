68a8f7b3fe35b5a3903f39c83d68d88872738a47
Edenilson Souza <edenilson.sza@gmail.com>
2024-03-07 18:33:50 -0300

    fix: corrige erro de CORS

--- src/infra/http/HttpServer.ts
+++ src/infra/http/HttpServer.ts
@@ -11,6 +11,13 @@ export class ExpressAdapter implements HttpServer {
     constructor() {
         this.app = express();
         this.app.use(express.json());
+
+        this.app.use((req: any, res: any, next: any) => {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+            next();
+        });
     }
 
     register(method: string, url: string, callback: Function): void {
