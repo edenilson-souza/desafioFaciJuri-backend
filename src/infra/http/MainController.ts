ca8e7ccd024c730df644e35661f0f1f2ecfa20b1
Edenilson Souza <edenilson.sza@gmail.com>
2024-03-08 02:19:12 -0300

    style: adiciona tipagem as rotas

--- src/infra/http/MainController.ts
+++ src/infra/http/MainController.ts
@@ -1,3 +1,4 @@
+import { Request } from "express";
 import HttpServer from "./HttpServer";
 import Signup from "../../application/usecase/Signup";
 import Registry, { inject } from "../di/Registry";
@@ -14,27 +15,27 @@ export default class MainController {
             return output;
         });
 
-        httpServer.register("get", "/accounts/:page/:limit", async function ({ body, params, query }: any) {
+        httpServer.register("get", "/accounts/:page/:limit", async function ({ body, params, query }: Request) {
             const output = await registry.inject("getAllAccount").execute(params.page, params.limit, query);
             return output;
         });
 
-        httpServer.register("get", "/accounts/:accountId", async function ({ body, params, query }: any) {
+        httpServer.register("get", "/accounts/:accountId", async function ({ body, params, query }: Request) {
             const output = await registry.inject("getAccount").execute(params.accountId);
             return output;
         });
 
-        httpServer.register("put", "/accounts/:accountId", async function ({ body, params, query }: any) {
+        httpServer.register("put", "/accounts/:accountId", async function ({ body, params, query }: Request) {
             const output = await registry.inject("updateAccount").execute(params.accountId, body);
             return output;
         });
 
-        httpServer.register("delete", "/accounts/:accountId", async function ({ body, params, query }: any) {
+        httpServer.register("delete", "/accounts/:accountId", async function ({ body, params, query }: Request) {
             const output = await registry.inject("deleteAccount").execute(params.accountId);
             return output;
         });
 
-        httpServer.register("get", "/rotas", async function ({ body, params, query }: any) {
+        httpServer.register("get", "/rotas", async function ({ body, params, query }: Request) {
             const output = await registry.inject("rotas").execute();
             return output;
         });
