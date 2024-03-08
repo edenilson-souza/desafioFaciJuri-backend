5e5b2bd21f94fcb41e0cb2373e6888d4bbf07e1a
Edenilson Souza <edenilson.sza@gmail.com>
2024-03-07 22:38:30 -0300

    feat: adiciona cords x e y no banco de dados

--- create.sql
+++ create.sql
@@ -9,15 +9,8 @@ create table desfacijuri.account (
 	name text not null,
 	email text not null,
 	phone text not null,
-    created_at timestamp default NOW(),
-    updated_at timestamp
-);
-
-create table desfacijuri.endereco (
-	endereco_id uuid primary key,
-    account_id uuid not null references desfacijuri.account(account_id),
-	x numeric not null,
-	y numeric not null,
+    cordx numeric not null,
+    cordy numeric not null,
     created_at timestamp default NOW(),
     updated_at timestamp
 );
